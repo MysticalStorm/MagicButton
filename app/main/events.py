6b301d4b5d3b8e76cb876d8e41e8bd125c98b3f8
Vladislav Chebotaryov <cvl@ciklum.com>
2018-10-05 18:42:33 +0300

    Add return value for events

--- app/main/events.py
+++ app/main/events.py
@@ -1,8 +1,7 @@
-from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
 
 @socketio.on('my event')
 def joined(json):
-    print("SOCKET EVENT")
-    print('received json: ' + str(json))
+    emit('my response', json)
+    return 'OK'
