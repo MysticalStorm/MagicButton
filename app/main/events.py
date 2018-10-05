d46eed42d31d30f4e0c7dad32f562127ad9c8352
Vladislav Chebotaryov <cvl@ciklum.com>
2018-10-05 18:58:09 +0300

    Refactoring

--- app/main/events.py
+++ app/main/events.py
@@ -1,7 +1,15 @@
-from flask_socketio import emit, join_room, leave_room
+from flask_socketio import emit
 from .. import socketio
 
-@socketio.on('my event')
-def joined(json):
-    emit('my response', json)
-    return 'OK'
+clicks = 0
+
+@socketio.on('connect')
+def joined():
+    print "User connected"
+
+@socketio.on('click')
+def click():
+    global clicks
+    clicks = clicks + 1
+
+    return clicks
